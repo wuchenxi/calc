b3801a4ea04953a01c0795ba8d3565afe02deeec
Chenxi Wu <wuchenxi2013@gmail.com>
2015-11-13 19:03:02 -0500

    add memory and test cases

--- cal.cpp
+++ cal.cpp
@@ -6,14 +6,16 @@ using std::tuple;
 using std::tie;
 using std::make_tuple;
 
+//memory
+double memory[1024*128];
+
+
 cal::calc_exp::calc_exp(std::string s):expr(s),result(0),done(false)
 {}
 
 int cal::calc_exp::set_exp(std::string s){expr=s;done=false;return 0;}
 
 std::string cal::calc_exp::get_exp(){return expr;}
-
-char* c;
 
 tuple<double, char*> eval(tuple<double,char*>,int);
 
@@ -33,6 +35,12 @@ tuple<double,char*> num(char* c){
   case 'l':c++;tie(r,c)=num(c);r=std::log(r);break;
   //c() means cos
   case 'c':c++;tie(r,c)=num(c);r=std::cos(r);break;
+  //$() read from memory
+  case '$':c++;{tie(r,c)=num(c);
+      int addr=(int)r;
+      if(0<=addr&&addr<1024*128)
+      r=memory[addr];}break;
+  //negative sign
   case '-':c++;tie(r,c)=num(c);r*=-1;break;
   default: while(c[0]>='0'&&c[0]<='9'){
       r=r*10+(c[0]-'0');c++;}
@@ -47,12 +55,16 @@ tuple<double,char*> num(char* c){
 
 inline int oplev(char c){
   switch(c){
-  case '+':return 0;
-  case '-':return 0;
-  case '*':return 1;
-  case '/':return 1;
-  case '%':return 1;
-  case '^':return 2;
+  case ';':return 0;
+  case ':':return 1;//assignment operator: value:address
+  case '>':return 2;
+  case '<':return 2;
+  case '+':return 3;
+  case '-':return 3;
+  case '*':return 4;
+  case '/':return 4;
+  case '%':return 4;
+  case '^':return 5;
   default: return -1;}
 }
 
@@ -67,13 +79,20 @@ tuple<double,char*> eval(tuple<double,char*> cur,int lev)
 	tie(rhs,c)=num(c);
     while(oplev(c[0])>oplev(op))
       tie(rhs,c)=eval(make_tuple(rhs,c),oplev(c[0]));
-    if(op=='+')r+=rhs;
-    else if(op=='-')r-=rhs;
-    else if(op=='*')r*=rhs;
-    else if(op=='/')r/=rhs;
-    else if(op=='^')r=std::pow(r,rhs);
-    else if(op=='%')r=((long)r)%((long)rhs);
-      }
+    switch(op){
+    case '+':r+=rhs;break;
+    case '-':r-=rhs;break;
+    case '*':r*=rhs;break;
+    case '/':r/=rhs;break;
+    case '^':r=std::pow(r,rhs);break;
+    case '%':r=((long)r)%((long)rhs);break;
+    case '>':r=(r>rhs);break;
+    case '<':r=(r<rhs);break;
+    case ':':{int addr=(int)rhs;
+	if(addr>=0&&addr<1024*128)memory[addr]=r;} break;
+    case ';':r=rhs;break;
+    }
+}
   return make_tuple(r,c);  
 }
 
