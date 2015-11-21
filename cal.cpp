4f857b310ebb8c938331136dea9bdbb73c69f85d
Chenxi Wu <wuchenxi2013@gmail.com>
2015-11-21 15:36:33 -0500

    bug fix

--- cal.cpp
+++ cal.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <tuple>
 #include <unordered_map>
+#include <cctype>
+//#include <iostream>
 
 //size of memory
 const int N=1024*128;
@@ -22,15 +24,19 @@ int cal::calc_exp::set_exp(std::string s){expr=s;done=false;return 0;}
 
 std::string cal::calc_exp::get_exp(){return expr;}
 
+//skip space, tab and \n
+void next(char* &c){
+  while(isspace(*c))c++;
+}
 
 //Binary operators
 tuple<double, char*> eval(tuple<double,char*>,int);
 
 //Unary operators
-tuple<double,char*> num(char* c){
+tuple<double,char*> num(char* c){next(c);
   double r=0;
   switch(c[0]){
-  case '(':c++;tie(r,c)=eval(num(c),0);c++;break;
+  case '(':c++;tie(r,c)=eval(num(c),0);if(c[0]==')')c++;break;
   //P means \pi
   case 'P':c++;r=M_PI;break;
   //E means e
@@ -64,6 +70,7 @@ tuple<double,char*> num(char* c){
       dec/=10;r+=dec*(c[0]-'0');
       c++;}
   }}
+  next(c);
   return make_tuple(r,c);}
 
 //precedence of binary operators
@@ -89,6 +96,7 @@ tuple<double,char*> eval(tuple<double,char*> cur,int lev)
   char* c;
   double r;
   tie(r,c)=cur;
+  next(c);
   while(lev<=oplev(c[0]))
     {char op=c[0];c++;
       double rhs;
@@ -108,6 +116,7 @@ tuple<double,char*> eval(tuple<double,char*> cur,int lev)
     case ';':r=rhs;break;
     }
 }
+  next(c);
   return make_tuple(r,c);  
 }
 
